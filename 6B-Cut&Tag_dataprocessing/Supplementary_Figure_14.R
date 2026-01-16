74dafab25ea9c61f35a413dd49d85402b0e99ee0
sblab-informatics <157594438+sblab-informatics@users.noreply.github.com>
2026-01-16 15:41:02 +0000

    Update Supplementary_Figure_14.R

--- 6B-Cut&Tag_dataprocessing/Supplementary_Figure_14.R
+++ 6B-Cut&Tag_dataprocessing/Supplementary_Figure_14.R
@@ -1,15 +1,14 @@
+############################################
+cat header.txt
+Grey	Red
+############################################
+
+
 #install.packages("tidyverse")
 require(reshape2)
 library(ggplot2)
 
-
-
-grep 'H3K4Me1' H3K4Me1_enrichment.20x.hmc.poised.bed | awk '{print $4*100}' >q
-grep 'Others' H3K4Me1_enrichment.20x.hmc.poised.bed | awk '{print $4*100}' >p
-paste p q >n
-cat header n >H3K4Me1_enrichment.20x.hmc.poised.1.txt
-
-dat<- read.table("H3K4Me1_enrichment.20x.hmc.poised.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K4Me1_enrichment.20x.hmc.poised.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$H3K4Me1)
 g <- summary(df$Others)
@@ -21,19 +20,15 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","Poised (H3K4Me1) CpG")
 
 pdf("H3K4Me1_enrichment.20x.hmc.poised.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("Poised (H3K4Me1) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("Poised (H3K4Me1) CpG" = '#96CA8D',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
 
 ############################################
 
-grep 'H3K4Me1' H3K4Me1_enrichment.20x.mc.poised.bed | awk '{print $4*100}' >q
-grep 'Others' H3K4Me1_enrichment.20x.mc.poised.bed | awk '{print $4*100}' >p
-paste p q >n
-cat header n >H3K4Me1_enrichment.20x.mc.poised.1.txt
 
-dat<- read.table("H3K4Me1_enrichment.20x.mc.poised.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K4Me1_enrichment.20x.mc.poised.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$H3K4Me1)
 g <- summary(df$Others)
@@ -46,19 +41,15 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","Poised (H3K4Me1) CpG")
 
 pdf("H3K4Me1_enrichment.20x.mc.poised.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("Poised (H3K4Me1) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("Poised (H3K4Me1) CpG" = '#45972E',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
 
 ############################################
 
-grep 'H3K27Me3' H3K27Me3_enrichment.20x.hmc.poised.bed | awk '{print $4*100}' >q
-grep 'Others' H3K27Me3_enrichment.20x.hmc.poised.bed | awk '{print $4*100}' >p
-paste p q >n
-cat header n >H3K27Me3_enrichment.20x.hmc.poised.1.txt
 
-dat<- read.table("H3K27Me3_enrichment.20x.hmc.poised.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K27Me3_enrichment.20x.hmc.poised.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$H3K27Me3)
 g <- summary(df$Others)
@@ -71,7 +62,7 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","Poised (H3K27Me3) CpG")
 
 pdf("H3K27Me3_enrichment.20x.hmc.poised.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("Poised (H3K27Me3) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("Poised (H3K27Me3) CpG" = '#E98380',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
@@ -79,12 +70,8 @@ dev.off()
 
 ############################################
 
-grep 'H3K27Me3' H3K27Me3_enrichment.20x.mc.poised.bed | awk '{print $4*100}' >q
-grep 'Others' H3K27Me3_enrichment.20x.mc.poised.bed | awk '{print $4*100}' >p
-paste p q >n
-cat header n >H3K27Me3_enrichment.20x.mc.poised.1.txt
 
-dat<- read.table("H3K27Me3_enrichment.20x.mc.poised.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K27Me3_enrichment.20x.mc.poised.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$H3K27Me3)
 g <- summary(df$Others)
@@ -97,7 +84,7 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","Poised (H3K27Me3) CpG")
 
 pdf("H3K27Me3_enrichment.20x.mc.poised.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("Poised (H3K27Me3) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("Poised (H3K27Me3) CpG" = '#EA3323',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
@@ -105,12 +92,8 @@ dev.off()
 
 ############################################
 
-grep 'H3K27ac' H3K27ac_enrichment.20x.hmc.active.bed | awk '{print $4*100}' >q
-grep 'Others' H3K27ac_enrichment.20x.hmc.active.bed | awk '{print $4*100}' >p
-paste p q >n
-cat header n >H3K27ac_enrichment.20x.hmc.active.1.txt
 
-dat<- read.table("H3K27ac_enrichment.20x.hmc.active.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K27ac_enrichment.20x.hmc.active.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$H3K27ac)
 g <- summary(df$Others)
@@ -123,19 +106,15 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","active (H3K27ac) CpG")
 
 pdf("H3K27ac_enrichment.20x.hmc.active.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K27ac) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K27ac) CpG" = '#5B5DAD',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
 
 ############################################
 
-grep 'H3K27ac' H3K27ac_enrichment.20x.mc.active.bed | awk '{print $4*100}' >q
-grep 'Others' H3K27ac_enrichment.20x.mc.active.bed | awk '{print $4*100}' >p
-paste p q >n
-cat header n >H3K27ac_enrichment.20x.mc.active.1.txt
 
-dat<- read.table("H3K27ac_enrichment.20x.mc.active.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K27ac_enrichment.20x.mc.active.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$H3K27ac)
 g <- summary(df$Others)
@@ -148,7 +127,7 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","active (H3K27ac) CpG")
 
 pdf("H3K27ac_enrichment.20x.mc.active.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K27ac) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K27ac) CpG" = '#2629CE',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
@@ -156,12 +135,8 @@ dev.off()
 ############################################
 
 
-grep 'H3K4Me1' H3K4Me1_enrichment.20x.hmc.active.bed | awk '{print $4*100}' >q
-grep 'Others' H3K4Me1_enrichment.20x.hmc.active.bed | awk '{print $4*100}' >p
-paste p q >n
-cat header n >H3K4Me1_enrichment.20x.hmc.active.1.txt
 
-dat<- read.table("H3K4Me1_enrichment.20x.hmc.active.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K4Me1_enrichment.20x.hmc.active.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$H3K4Me1)
 g <- summary(df$Others)
@@ -174,19 +149,15 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","active (H3K4Me1) CpG")
 
 pdf("H3K4Me1_enrichment.20x.hmc.active.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K4Me1) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K4Me1) CpG" = '#96CA8D',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
 
 ############################################
 
-grep 'H3K4Me1' H3K4Me1_enrichment.20x.mc.active.bed | awk '{print $4*100}' >q
-grep 'Others' H3K4Me1_enrichment.20x.mc.active.bed | awk '{print $4*100}' >p
-paste p q >n
-cat header n >H3K4Me1_enrichment.20x.mc.active.1.txt
 
-dat<- read.table("H3K4Me1_enrichment.20x.mc.active.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K4Me1_enrichment.20x.mc.active.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$H3K4Me1)
 g <- summary(df$Others)
@@ -199,7 +170,7 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","active (H3K4Me1) CpG")
 
 pdf("H3K4Me1_enrichment.20x.mc.active.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K4Me1) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K4Me1) CpG" = '#45972E',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
@@ -220,7 +191,7 @@ I############################################
 
 
 
-dat<- read.table("H3K4Me1_enrichment.20x.mc.primed.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K4Me1_enrichment.20x.mc.primed.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$Red)
 g <- summary(df$Grey)
@@ -233,14 +204,14 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","active (H3K4Me1) CpG")
 
 pdf(" H3K4Me1_enrichment.20x.mc.primed.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K4Me1) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K4Me1) CpG" = '#45972E',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
 
 
 ############################################
 
-dat<- read.table("H3K4Me1_enrichment.20x.hmc.primed.1.txt", fill=TRUE, header=T)
+dat<- read.table("H3K4Me1_enrichment.20x.hmc.primed.txt", fill=TRUE, header=T)
 df = data.frame(dat)
 r <- summary(df$Red)
 g <- summary(df$Grey)
@@ -253,12 +224,7 @@ sample_main <- melt(df)
 levels(sample_main$variable) <- c("Other CpG","active (H3K4Me1) CpG")
 
 pdf(" H3K4Me1_enrichment.20x.hmc.primed.boxplot_Violin.pdf")
-p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K4Me1) CpG" = 'tomato2',"Other CpG" = 'grey'))
+p <- ggplot(sample_main, aes(variable, value)) + geom_violin((aes(fill=variable))) + scale_fill_manual(values = c("active (H3K4Me1) CpG" = '#96CA8D',"Other CpG" = 'grey'))
 p + theme_classic()
 dev.off()
-
-
-
-
-############################################
 
