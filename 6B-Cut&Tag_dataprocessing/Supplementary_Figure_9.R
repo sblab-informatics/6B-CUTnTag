357e055c87bcab2cdefd47c404549b3cac75fd9b
sblab-informatics <157594438+sblab-informatics@users.noreply.github.com>
2026-01-16 16:13:39 +0000

    Update Supplementary_Figure_9.R

--- 6B-Cut&Tag_dataprocessing/Supplementary_Figure_9.R
+++ 6B-Cut&Tag_dataprocessing/Supplementary_Figure_9.R
@@ -5,8 +5,8 @@ head(df)
 y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
-png("H3K27ac_6L_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "6L mc (CpGs shared)",xlab = "H3K27ac enrichment", ylab = "6L (mc Methylation %)", col = "blue3", pch = ".")
+png("H3K27ac_6B_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
+plot(df2, main = "6B mc (CpGs shared)",xlab = "H3K27ac enrichment", ylab = "6B (mc Methylation %)", col = "blue3", pch = ".")
 mtext("n=42313", side=3)
 dev.off()
 
@@ -18,8 +18,8 @@ head(df)
 y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
-png("H3K27ac_6L_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "6L hmc (CpGs shared)",xlab = "H3K27ac enrichment", ylab = "6L (hmc Methylation %)", col = "dodgerblue3", pch = ".")
+png("H3K27ac_6B_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
+plot(df2, main = "6B hmc (CpGs shared)",xlab = "H3K27ac enrichment", ylab = "6B (hmc Methylation %)", col = "dodgerblue3", pch = ".")
 mtext("n=42313", side=3)
 dev.off()
 
@@ -33,7 +33,7 @@ y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
 png("H3K27ac_WG_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "WG mc (CpGs shared with 6L)",xlab = "H3K27ac enrichment", ylab = "WG (mc Methylation %)", col = "blue3", pch = ".")
+plot(df2, main = "WG mc (CpGs shared with 6B)",xlab = "H3K27ac enrichment", ylab = "WG (mc Methylation %)", col = "blue3", pch = ".")
 mtext("n=42313", side=3)
 dev.off()
 
@@ -46,7 +46,7 @@ y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
 png("H3K27ac_WG_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "WG hmc (CpGs shared with 6L)",xlab = "H3K27ac enrichment", ylab = "WG (hmc Methylation %)", col = "dodgerblue3", pch = ".")
+plot(df2, main = "WG hmc (CpGs shared with 6B)",xlab = "H3K27ac enrichment", ylab = "WG (hmc Methylation %)", col = "dodgerblue3", pch = ".")
 mtext("n=42313", side=3)
 dev.off()
 
@@ -61,8 +61,8 @@ head(df)
 y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
-png("H3K4Me1_6L_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "6L mc (CpGs shared)",xlab = "H3K4Me1 enrichment", ylab = "6L (mc Methylation %)", col = "green3", pch = ".")
+png("H3K4Me1_6B_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
+plot(df2, main = "6B mc (CpGs shared)",xlab = "H3K4Me1 enrichment", ylab = "6B (mc Methylation %)", col = "green3", pch = ".")
 mtext("n=75292", side=3)
 dev.off()
 
@@ -74,8 +74,8 @@ head(df)
 y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
-png("H3K4Me1_6L_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "6L hmc (CpGs shared)",xlab = "H3K4Me1 enrichment", ylab = "6L (hmc Methylation %)", col = "palegreen1", pch = ".")
+png("H3K4Me1_6B_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
+plot(df2, main = "6B hmc (CpGs shared)",xlab = "H3K4Me1 enrichment", ylab = "6B (hmc Methylation %)", col = "palegreen1", pch = ".")
 mtext("n=75292", side=3)
 dev.off()
 
@@ -89,7 +89,7 @@ y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
 png("H3K4Me1_WG_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "WG mc (CpGs shared with 6L)",xlab = "H3K4Me1 enrichment", ylab = "WG (mc Methylation %)", col = "green3", pch = ".")
+plot(df2, main = "WG mc (CpGs shared with 6B)",xlab = "H3K4Me1 enrichment", ylab = "WG (mc Methylation %)", col = "green3", pch = ".")
 mtext("n=75292", side=3)
 dev.off()
 
@@ -102,7 +102,7 @@ y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
 png("H3K4Me1_WG_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "WG hmc (CpGs shared with 6L)",xlab = "H3K4Me1 enrichment", ylab = "WG (hmc Methylation %)", col = "palegreen1", pch = ".")
+plot(df2, main = "WG hmc (CpGs shared with 6B)",xlab = "H3K4Me1 enrichment", ylab = "WG (hmc Methylation %)", col = "palegreen1", pch = ".")
 mtext("n=75292", side=3)
 dev.off()
 
@@ -117,8 +117,8 @@ head(df)
 y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
-png("H3K27Me3_6L_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "6L mc (CpGs shared)",xlab = "H3K27Me3 enrichment", ylab = "6L (mc Methylation %)", col = "indianred1", pch = ".")
+png("H3K27Me3_6B_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
+plot(df2, main = "6B mc (CpGs shared)",xlab = "H3K27Me3 enrichment", ylab = "6B (mc Methylation %)", col = "indianred1", pch = ".")
 mtext("n=27381", side=3)
 dev.off()
 
@@ -130,8 +130,8 @@ head(df)
 y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
-png("H3K27Me3_6L_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "6L hmc (CpGs shared)",xlab = "H3K27Me3 enrichment", ylab = "6L (hmc Methylation %)", col = "lightsalmon", pch = ".")
+png("H3K27Me3_6B_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
+plot(df2, main = "6B hmc (CpGs shared)",xlab = "H3K27Me3 enrichment", ylab = "6B (hmc Methylation %)", col = "lightsalmon", pch = ".")
 mtext("n=27381", side=3)
 dev.off()
 
@@ -145,7 +145,7 @@ y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
 png("H3K27Me3_WG_shared_cpg_mc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "WG mc (CpGs shared with 6L)",xlab = "H3K27Me3 enrichment", ylab = "WG (mc Methylation %)", col = "indianred1", pch = ".")
+plot(df2, main = "WG mc (CpGs shared with 6B)",xlab = "H3K27Me3 enrichment", ylab = "WG (mc Methylation %)", col = "indianred1", pch = ".")
 mtext("n=27381", side=3)
 dev.off()
 
@@ -158,6 +158,6 @@ y <- log2(df["Val1"])
 x <- (df["Val2"])*100
 df2 = data.frame(y,x)
 png("H3K27Me3_WG_shared_cpg_hmc.png", res = 300, width = 5, height = 4, units = 'in')
-plot(df2, main = "WG hmc (CpGs shared with 6L)",xlab = "H3K27Me3 enrichment", ylab = "WG (hmc Methylation %)", col = "lightsalmon", pch = ".")
+plot(df2, main = "WG hmc (CpGs shared with 6B)",xlab = "H3K27Me3 enrichment", ylab = "WG (hmc Methylation %)", col = "lightsalmon", pch = ".")
 mtext("n=27381", side=3)
 dev.off()
